2ae07963ca1bbdc353c9958a43526d803534d631
Michal Bobula <michal.bobula@microsoft.wsei.edu.pl>
2022-10-15 11:40:27 +0200

    display 0

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,19 @@
-let result = ""
+let result = "";
 for (var i = 0; i <= 100; i++) {
-    while (!(i % 3)) {
+    while (!(i % 3) && i != 0) {
         result += "fizz";
         break;
     }
 
-    while (!(i % 5)) {
+    while (!(i % 5) && i != 0) {
         result += "buzz";
         break;
     }
 
-    while (i % 3 && i % 5) {
+    while (i % 3 && i % 5 || i == 0) {
         result += i;
         break;
     }
-    result += ", "
+    result += ", ";
 }
-console.log(result)
+console.log(result);
